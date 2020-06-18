58cce88d29beb03a3d7fb1ee470129f1e247a09e
Nik Rowe <NikRowe7@Gmail.com>
2020-06-18 01:02:15 -0700

    Testing GET function

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express')
 // Set up EXPRESS app //
 const app = express ()
 
+app.get('/api', (req, res) => {
+    console.log('GET Request')
+    res.send({ name: 'Pikachu'})
+})
 
 // Listen for requests //
 app.listen(process.env.port || 4000, () =>{
