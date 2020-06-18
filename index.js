f06243b080c17292b5a99a69e07eb61b8942fd66
Nik Rowe <NikRowe7@Gmail.com>
2020-06-18 02:30:31 -0700

    Connect to MongoDB & Saving Data

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 // Import Routes //
 const routes = require('./routes/api')
 
 // Set up EXPRESS app //
-const app = express ()
+const app = express()
+
+// Connect to MongoDB //
+mongoose.connect('mongodb://localhost/pokemongo', {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+mongoose.Promise = global.Promise
 
 // BP parses the client body and attaches it to the req param //
 app.use(bodyParser.json())
@@ -14,6 +24,6 @@ app.use(bodyParser.json())
 app.use('/api', routes)
 
 // Listen for requests //
-app.listen(process.env.port || 4000, () =>{
+app.listen(process.env.port || 4000, () => {
     console.log('now listening for requests')
 })
--- routes/api.js
+++ routes/api.js
@@ -1,26 +1,25 @@
 const express = require('express')
 const router = express.Router()
+const Pokemon = require('../models/pokemon')
 
 // GET list of pokemon from the db
 router.get('/pokemon', (req, res) => {
-    res.send({ type : 'GET'})
+    res.send({ type: 'GET' })
 })
 // POST(add) a new pokemon to the db
 router.post('/pokemon', (req, res) => {
-    console.log(req.body)
-    res.send({ 
-        type : 'POST',
-        name: req.body.name,
-        rank: req.body.type
-    })
+    Pokemon.create(req.body) // mongoose method returning promise to get body data and save to db with the Pokemon Schema
+        .then((pokemon) => {
+            res.send(pokemon)
+        })
 })
 // PUT(update) a pokemon in the db
 router.put('/pokemon/:id', (req, res) => {
-    res.send({ type : 'PUT'})
+    res.send({ type: 'PUT' })
 })
 // DELETE pokemon from the db
 router.delete('/pokemon/:id', (req, res) => {
-    res.send({ type : 'DELETE'})
+    res.send({ type: 'DELETE' })
 })
 
 module.exports = router
