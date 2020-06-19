d87d84d46920143b7753ac22116ba621ce2259ae
Nik Rowe <NikRowe7@Gmail.com>
2020-06-18 19:58:16 -0700

    GeoJSON for pokemon locations

--- models/pokemon.js
+++ models/pokemon.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+// Create Geo Location Schema //
+const GeoSchema = new Schema({
+    type: {
+        type: String,
+        default: "Point"
+    },
+    coordinates: {
+        type: [Number],
+        index: "2dsphere"
+    }
+})
+
 // Create pokemon Schema & model //
 const PokemonSchema = new Schema({
     name: {
@@ -13,7 +25,8 @@ const PokemonSchema = new Schema({
     nearBy: {
         type: Boolean,
         default: false
-    }
+    },
+    geometry: GeoSchema
 }, {
     timestamps: true
     // add in location //
